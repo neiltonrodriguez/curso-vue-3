91e9eea8d5b2044eacb1841a017d2db5c82efad5
neiltonmadeira <neilton.silva@madeiramadeira.com.br>
2023-06-04 23:38:44 -0300

    mais estudos de vueJs

---
+++ frontend/src/components/TesteComponent.vue
@@ -0,0 +1,6 @@
+<template>
+   <h2 id="teste2">
+    Component Teste
+   </h2>
+</template>
+
---
+++ frontend/src/views/ProductsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TesteComponent from '../components/TesteComponent.vue'
+</script>
+<template>
+   <h2 id="teste2">TESTANDO ROTAS PRODUCTS</h2>
+   <TesteComponent/>
+</template>
+
+<style scoped>
+@import "@/assets/teste.css"
+</style>
